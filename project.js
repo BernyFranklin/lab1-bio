23a11e50b9d49024d8d03afd7027d5f95905e635
FrankBernal <frankbernal@mail.fresnostate.edu>
2025-09-11 19:27:08 -0700

    added types to objects.

--- project.js
+++ project.js
@@ -4,24 +4,27 @@ let projects = [
       description:
          "This is the eye tracking tool to be used for the Psych Department in diagnosing ADHD with objective data.",
       link: "#",
+      type: "school",
    },
    {
       title: "SIR Model Infection Simulation",
       description:
          "This project used the Susceptible, Infected, Recovery (SIR) model to simulate infection spread in a population. The project was written in Python and included user entered parameters for Population Size, Intitial Infected, Infection Rate, Recovery Time, and Movement Speed. The agents were color coded dots: Blue (Susceptible), Red (Infected), and Green (Recovered).",
       link: "https://github.com/BernyFranklin/P1_6-Infection",
+      type: "school",
    },
    {
       title: "Email Generator",
       description:
          "This project involved making a tool to generate common emails the Veterans Resource Center sends out to new students seeking to use their Veteran Affairs Education Benefits. I was the sole contributor and created it to make my office work easier and more efficient.",
       link: "https://bernyfranklin.github.io/vrcEmailGen/index.html",
+      type: "personal",
    },
 ];
 
 function listProjects(projects, sectionId) {
    const section = document.getElementById(sectionId);
-   //section.innerHTML = "";
+   section.innerHTML = "";
 
    projects.forEach((project) => {
       const card = document.createElement("article");
